f44864e2495ccb377613c282be49b01061f853e9
Goytai <dev.goytai@gmail.com>
2021-05-31 09:22:49 -0300

    Chore: config the craco

---
+++ craco.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  webpack: {
+    alias: {
+      '~': path.resolve(__dirname, 'src/'),
+      '@components': path.resolve(__dirname, 'src/components/'),
+      '@images': path.resolve(__dirname, 'src/assets/images/'),
+      '@icons': path.resolve(__dirname, 'src/assets/icons/')
+    }
+  }
+};
